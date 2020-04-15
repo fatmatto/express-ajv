df1068a40da514a78f2402068e35e58332db9522
Mattia Alfieri <cikkense@gmail.com>
2020-04-15 14:45:24 +0200

    refactor: simplify errors.

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 'use strict'
 
 const Ajv = require('ajv')
-
+const Errors = require('throwable-http-errors')
 /**
  * Returns a middleware with compiled ajv validators
  * @param  {Object} routeSchema An object with ajv schemas {body: Schema1, query: Schema2, prams: Schema3}
  */
-let validateRoute = (routeSchema) => {
+const validateRoute = routeSchema => {
   let ajv = null
 
-  let validators = {}
+  const validators = {}
 
   // Compiling query schema beforehand
-  if (routeSchema.hasOwnProperty('query')) {
+  if (Object.prototype.hasOwnProperty.call(routeSchema, 'query')) {
     ajv = new Ajv()
     validators.query = ajv.compile(routeSchema.query)
   }
 
   // Compiling params schema beforehand
-  if (routeSchema.hasOwnProperty('params')) {
+  if (Object.prototype.hasOwnProperty.call(routeSchema, 'params')) {
     // We coerce types on params because they cann only be strings
     // since they are part of an url
-    ajv = new Ajv({coerceTypes: true})
+    ajv = new Ajv({ coerceTypes: true })
     validators.params = ajv.compile(routeSchema.params)
   }
 
   // Compiling body schema beforehand
-  if (routeSchema.hasOwnProperty('body')) {
+  if (Object.prototype.hasOwnProperty.call(routeSchema, 'body')) {
     ajv = new Ajv()
     validators.body = ajv.compile(routeSchema.body)
   }
@@ -36,24 +36,24 @@ let validateRoute = (routeSchema) => {
   return (req, res, next) => {
     let validation = null
 
-    if (validators.hasOwnProperty('params')) {
-      validation = validators.params(req.params)
+    if (Object.prototype.hasOwnProperty.call(validators, 'params')) {
+      validation = ajv.validate(routeSchema.params, req.params)
       if (!validation) {
-        return next(validators.params.errors)
+        return next(new Errors.BadRequest(`Request url parameters validation failed: ${ajv.errorsText()}`))
       }
     }
 
-    if (validators.hasOwnProperty('query')) {
-      validation = validators.query(req.query)
+    if (Object.prototype.hasOwnProperty.call(validators, 'query')) {
+      validation = ajv.validate(routeSchema.query, req.query)
       if (!validation) {
-        return next(validators.query.errors)
+        return next(new Errors.BadRequest(`Request query validation failed: ${ajv.errorsText()}`))
       }
     }
 
-    if (validators.hasOwnProperty('body')) {
-      validation = validators.body(req.body)
+    if (Object.prototype.hasOwnProperty.call(validators, 'body')) {
+      validation = ajv.validate(routeSchema.body, req.body)
       if (!validation) {
-        return next(validators.body.errors)
+        return next(new Errors.BadRequest(`Request body validation failed: ${ajv.errorsText()}`))
       }
     }
 
